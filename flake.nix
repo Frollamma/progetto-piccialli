b8ac6197bb1d567eb230c72a33f23e3ac2a4b4bd
Frollamma <fral01@protonmail.com>
2024-05-22 08:26:30 +0200

    Update Nix flakes

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Development environment for Python 3.11 scripts and packages";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2311.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+          (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          name = "python 3.11 development shell";
+          packages = with pkgs;
+            [
+              python311
+              python311
+              poetry
+            ] ++ (with pkgs.python311Packages; [
+              pip
+              icecream
+              black
+            ]);
+          shellHook = ''
+            echo
+            echo "Activated environment for Python 3.11 development"
+          '';
+        };
+      });
+    };
+}
